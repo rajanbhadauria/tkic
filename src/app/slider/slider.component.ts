d3b79f8ce99cef68ce8e205f8cded939b0b421db
Rajan Bhadauria <rajanbhadauria@gmail.com>
2024-04-08 22:17:29 +0530

    New images added

--- src/app/slider/slider.component.ts
+++ src/app/slider/slider.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SliderComponent implements OnInit {
 
-  images = ['assets/slider/1.jpeg', 'assets/slider/3.jpeg', 'assets/slider/2.jpeg'];
+  images = ['assets/slider/1.jpeg', 'assets/slider/2.jpeg', 'assets/slider/3.jpeg', 'assets/slider/4.jpeg', 'assets/slider/5.jpeg', 'assets/slider/6.jpeg', 'assets/slider/7.jpeg', 'assets/slider/8.jpeg'];
 
   ngOnInit(): void {
   }
